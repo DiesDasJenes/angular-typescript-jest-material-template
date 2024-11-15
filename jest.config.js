9295b913a7db38eac13d9b747e501c0c30c83c69
Philipp Ludewig <diesdas@philippludewig.de>
2024-11-15 09:33:51 -0500

    inital commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@shared/(.*)': '<rootDir>/src/app/shared/$1',
+  },
+  roots: ['<rootDir>/src'],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['./setup-jest.ts'],
+  collectCoverage: true,
+  coverageReporters: ['lcov'],
+  coverageDirectory: './coverage/jest',
+  collectCoverageFrom: ['**/*.ts']
+};
